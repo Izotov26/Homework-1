7a6fa0f1fded4ab54f541c0ba1d48392e6447ccd
Izotov26 <grigorizotovg@gmail.com>
2025-04-09 21:09:25 +0400

    Сделано задание 1

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args){
+        System.out.println("Задание #3");
+        int x =21;
+        int y= 4;
+        int q=x/y;
+        int w=x%y;
+        System.out.println("квадратный корень = " + Math.sqrt(x));
+        System.out.println(x + " / " + y + " = " + q + " остаток " + w);
+    }
+}
